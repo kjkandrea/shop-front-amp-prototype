6a83032cec22cdf2e6e3e2831dbb0bbe0e8174a3
jk kim <kjkandrea@gmail.com>
2021-04-03 16:14:23 +0900

    Add basic configs

---
+++ webpack-loader.js
@@ -0,0 +1,4 @@
+module.exports = function webpackLoader (content) {
+  console.log('♻️ webpack loader ♻️')
+  return content
+}
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: './src/app.js'
+  },
+  output: {
+    path: path.resolve('./dist'),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          path.resolve('./webpack-loader.js')
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        loader: 'url-loader',
+        options: {
+          publicPath: '../dist/',
+          name: '[name].[ext]?[hash]',
+          limit: 20000, // 20kb
+        },
+      },
+      {
+        test: /\.(js|jsx)$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      inject: 'body'
+    })
+  ]
+}
