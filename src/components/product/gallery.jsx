95830b96310e4a7c7ad09b21621b940f3ae84f8d
jk kim <kjkandrea@gmail.com>
2021-04-04 13:23:45 +0900

    Add tiny slider

--- src/components/product/gallery.jsx
+++ src/components/product/gallery.jsx
@@ -1,11 +1,17 @@
 import createElement from '../../utils/element'
 import { fitToScreen, square } from '../../assets/styles/common'
+import { tns } from 'tiny-slider/src/tiny-slider';
+import 'tiny-slider/src/tiny-slider.scss'
 
 class Gallery {
+  constructor() {
+    this.slider = null
+  }
+
   template(images) {
     return (
       <div>
-        <ul>
+        <ul id="slider">
           {images.map(({ name, src }) => (
             <li className={square('100%')}>
               <img className={fitToScreen} src={src} alt={name} />
@@ -15,8 +21,15 @@ class Gallery {
       </div>
     )
   }
+  sliderInit() {
+    const foo = tns({
+      container: '#slider',
+      items: 1,
+    });
+  }
   render(data) {
     document.getElementById('product-summary').append(this.template(data))
+    this.sliderInit()
   }
 }
 
